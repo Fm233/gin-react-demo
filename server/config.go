76d24a10bade37ba85ce7875e9cd4f10ee4f4144
Fm233 <255307978@qq.com>
2022-10-08 07:01:22 +0800

    change password

--- server/config.go
+++ server/config.go
@@ -4,4 +4,4 @@ const GetVideoSite = "https://api.bilibili.com/x/web-interface/view?bvid=%s"
 const MsUpperLimit = 150 * 1000
 const AdminPassword = "eStrA6PJ2LmFcjav74zkdo737eShvVAs"
 const SessionSecret = "hFxTEoohxfqqoEup0butuX08ZSOOPafx"
-const DBStr = "root:3rpdc9TpAllqSzLUMFkOglslBIbOSzWS@tcp(mysql:3306)/data?charset=utf8mb4&parseTime=True&loc=Local"
+const DBStr = "root:secret@tcp(mysql:3306)/data?charset=utf8mb4&parseTime=True&loc=Local"
