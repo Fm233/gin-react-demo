3c779bd21514130b58f4f083d4025732abf51dc1
Fm233 <255307978@qq.com>
2023-02-02 13:42:54 +0800

    修改提示语

--- client/src/features/apply/ProgressFail.tsx
+++ client/src/features/apply/ProgressFail.tsx
@@ -1,22 +1,25 @@
-import { Button, Result } from 'antd';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { Button, Result } from "antd";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const ProgressFail: React.FC = () => (
-    <Result
-        style={{ margin: '120px auto' }}
-        status="error"
-        title="审核未通过"
-        subTitle="请重新提交申请"
-        extra={[
-            <Link to="/apply">
-                <Button type="primary" key="console">重新申请</Button>
-            </Link>,
-            <Link to="/">
-                <Button key="buy">回到主页</Button>
-            </Link>,
-        ]}
-    />
+  <Result
+    style={{ margin: "120px auto" }}
+    status="error"
+    title="审核未通过"
+    subTitle="请换一个视频试试"
+    extra={[
+      <Link to="/apply">
+        <Button type="primary" key="console">
+          重新申请
+        </Button>
+      </Link>,
+      <Link to="/">
+        <Button key="buy">回到主页</Button>
+      </Link>,
+    ]}
+  />
 );
 
 export default ProgressFail;
+
